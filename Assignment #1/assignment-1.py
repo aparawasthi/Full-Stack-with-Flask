5112340c65227ba9971f1dc97fe6e38bd82cc521
Apar Awasthi <aparawasthi1@gmail.com>
2021-05-08 00:43:46 +0530

    Removed extra line from the code

--- Assignment #1/assignment-1.py
+++ Assignment #1/assignment-1.py
@@ -3,7 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
-
 def home():
     return ('<!DOCTYPE html><html lang="en"><head><title>My Flask App</title></head><body><h1>Hello World!!</h1></body></html>')
 
