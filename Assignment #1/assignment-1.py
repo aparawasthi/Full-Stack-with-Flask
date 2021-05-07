40309de5808807995b0b60f8a40a1403a7662ce2
Apar Awasthi <aparawasthi1@gmail.com>
2021-05-07 08:37:19 +0530

    My First Flask App

--- Assignment #1/assignment-1.py
+++ Assignment #1/assignment-1.py
@@ -1,1 +1,14 @@
-from flask import flask
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+
+def home():
+    return ('<!DOCTYPE html><html lang="en"><head><title>My Flask App</title></head><body><h1>Hello World!!</h1></body></html>')
+
+if __name__ == '__main__':
+    # Specifying Port for web application, Default port 5000
+    # Starting in debug mode to detect changes in file and restarting the server automatically
+    # app.run(port = 7000, debug = True)
+    app.run()
